e11878e64a95ac67e0bc1ad5175a4a2b278ae7fe
Nilox42 <jtmauerman@gmail.com>
2021-06-22 19:24:26 +0200

    working on server browser

---
+++ DuckHunt/GameClient/Assets/Multiplayer/MatchMaking/MatchMaker.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchMaker : MonoBehaviour
+{
+    public Canvas serverBrowser;
+    public InputField input;
+
+    public void searchforServer()
+    {
+        Debug.Log("Test");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
